7f174443dca82a38932d3904aebf3eabdfdfbded
codingwithahmed <aziahmedfarooq@gmail.com>
2020-12-10 00:36:04 +0500

    UI Sidebar 70%

---
+++ ui/src/component/Mid.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Mid.css'
+export default function Mid() {
+    return (
+        <div className="mid">
+            <div className="header">
+               <div className="header_right">
+
+               </div>
+            </div>
+        </div>
+    )
+}
--- ui/src/component/SideBar.js
+++ ui/src/component/SideBar.js
@@ -1,14 +1,33 @@
-import React from 'react'
 import './SideBar.css'
-export default function SideBar() {
-    return (
-        <div className="sidebar">
+import React, { Component } from 'react'
+
+export default class SideBar extends Component {
+
+    constructor(props){
+        super(props);
+        this.wrapperref = React.createRef();
+        this.state = {
+            
+        }   
+
+    }
+
+
+    handleclick(){
+        const wrapper = this.wrapperref.current;
+        wrapper.classList.toggle('t_1')
+    }
+
+    render() {
+        return (
+<div className="sidebar">
+
                 <div className="logo">
-                   <h1>Logo</h1>
+                   <h1>Adan</h1>
                 </div>
 
-                <div className="menu">
-                    <div className="menu_item">
+                <div  className="menu">
+                    <div ref={this.wrapperref} onClick={() => this.handleclick()} className="menu_item">
                    <h3>Dashoard</h3>
                    </div>
 
@@ -28,9 +47,19 @@ export default function SideBar() {
                    <h3>Setting</h3>
                    </div>
 
-                   
-                   
                 </div>
+
+                <div className="cards">
+                    <div className="card">
+                        <h3 style={{color:"white",textAlign:"center",fontWeight:100}}>Use our Prenium <br /> Feature now </h3>
+                        <button className="cards_btn">Get Prenium</button>
+                    </div>
+              
+                     
+                </div>
+                
         </div>
-    )
+        )
+    }
 }
+
