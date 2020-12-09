00c9c158a80675d4bd43411e4e0b75cc9631ab94
codingwithahmed <aziahmedfarooq@gmail.com>
2020-12-09 23:29:46 +0500

    UI Start

---
+++ ui/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import SideBar from './component/SideBar'
+function App() {
+  return (
+    <div className="app">
+         <SideBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ ui/src/component/SideBar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './SideBar.css'
+export default function SideBar() {
+    return (
+        <div className="sidebar">
+                <div className="logo">
+                   <h1>Logo</h1>
+                </div>
+
+                <div className="menu">
+                    <div className="menu_item">
+                   <h3>Dashoard</h3>
+                   </div>
+
+                   <div className="menu_item">
+                   <h3>Cards</h3>
+                   </div>
+
+                   <div className="menu_item">
+                   <h3>Prenium</h3>
+                   </div>
+
+                   <div className="menu_item">
+                   <h3>Profile</h3>
+                   </div>
+
+                   <div className="menu_item">
+                   <h3>Setting</h3>
+                   </div>
+
+                   
+                   
+                </div>
+        </div>
+    )
+}
